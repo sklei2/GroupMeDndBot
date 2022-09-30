333a37797d28ef378c65f8090f1aa85d60b7e4ce
sklei2 <sklei1151@gmail.com>
2022-09-29 19:43:00 -0600

    need to extend IOApp

--- bot/src/main/scala/com/g5deathmarch/dndbot/Main.scala
+++ bot/src/main/scala/com/g5deathmarch/dndbot/Main.scala
@@ -2,7 +2,7 @@ package com.g5deathmarch.dndbot
 
 import cats.effect.{ExitCode, IO, IOApp}
 
-object Main {
+object Main extends IOApp {
   def run(args: List[String]) =
     Server.stream[IO].compile.drain.as(ExitCode.Success)
 }
